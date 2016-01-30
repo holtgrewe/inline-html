c6b73c073aa249188d6c3bad4ac036d59689eed5
Andreas Jung <yet@gmx.de>
2016-01-30 17:38:22 +0100

    added

---
+++ conv.py
@@ -0,0 +1,27 @@
+import os
+import base64
+import mimetypes
+import lxml.html
+
+
+def img_to_data(path):
+    """Convert a file (specified by a path) into a data URI."""
+    if not os.path.exists(path):
+        raise FileNotFoundError
+    mime, _ = mimetypes.guess_type(path)
+    with open(path, 'rb') as fp:
+        data = fp.read()
+        data64 = u''.join(base64.encodestring(data).splitlines())
+        return u'data:%s;base64,%s' % (mime, data64)
+
+
+html = open('in.html', 'rb').read()
+root = lxml.html.fromstring(html)
+
+for img in root.xpath('//img'):
+    src = img.attrib['src']
+    data = img_to_data(src)
+    img.attrib['src'] = data
+
+open('out.html', 'wb').write(lxml.html.tostring(root))
+
